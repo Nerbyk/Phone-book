a96331ee57a234d0eb40a2ecd9895bdaf176dd51
Nerbyk <danil.maximov2000@gmail.com>
2020-04-14 23:43:07 +0200

    Comments have been added.

--- Proj1.rb
+++ Proj1.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# this function checks input arguments
 def input_check
   if ARGV[0].count('a-zA-Z') > 0
     warn('Incorrect input. You are able to enter only digits')
@@ -10,17 +11,21 @@ def input_check
   end
 end
 
+# parsing contacts from input file from argumet
 def parse_contacts
   input = $stdin.read.downcase.split("\n")
+  # sorting input in specified format ( [["name", "number"] , ["name", "number"])
   input = Hash[*input].to_a
   input
 end
 
+# returns matched contacts by number
 def check_number(contacts)
   matched_contacts = contacts.map { |x| x.join.include?(ARGV[0]) ? x : nil }
   matched_contacts
 end
 
+# returns matched contacts by letters on phone keypad
 def check_names(contacts)
   keys_for_matches = generate_keyboard
   new_contacts = []
@@ -30,9 +35,13 @@ def check_names(contacts)
   new_contacts.flatten.compact.uniq
 end
 
+# returns all possible combinations of button letter with a help of specific permutation
 def generate_keyboard
+  # creates some type of phone keypad
   keyboard = %w[abc def ghi jkl mno pqrs tuv wxyz]
+  # getting the right combination of letters for a given input ARGV[0]
   template = ARGV[0].chars.map { |x| keyboard[x.to_i - 2].chars }
+  # getting array of all possible combinations by permutatio
   template = template.shift.product(*template).map(&:join)
   template
 end
